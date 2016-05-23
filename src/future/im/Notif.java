22999c23b0fe05b07b86afc8a13d7b9772f3834a
Ruslan <ruha9005@gmail.com>
2016-05-23 13:26:59 +0300

    Update Notif.java
    SDK-23 build support. (required to extend minimal sdk version to 16)

--- src/future/im/Notif.java
+++ src/future/im/Notif.java
@@ -13,8 +13,35 @@ public class Notif {
     public static int notifId = 654654;
 
     public static Notification getNotification(Context context) {
+        Notification n;
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                context,
+                0,
+                new Intent(context, Login.class),
+                0);
 
-        Notification n = new Notification(
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+            n = new Notification(
+                    R.drawable.stat_sample,
+                    "Think Consult Chat",
+                    System.currentTimeMillis());
+            try {
+                Method deprecatedMethod = n.getClass().getMethod("setLatestEventInfo", Context.class, CharSequence.class, CharSequence.class, PendingIntent.class);
+                deprecatedMethod.invoke(n, context, "Think & Consult IM", "Secure Communication...", pendingIntent);
+            } catch (NoSuchMethodException | IllegalAccessException | IllegalArgumentException
+                    | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        } else {
+            // Use new API
+            Notification.Builder builder = new Notification.Builder(context)
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(R.drawable.stat_sample)
+                    .setContentTitle("Think Consult Chat");
+            n = builder.build();
+        }
+
+/*        Notification n = new Notification(
                 R.drawable.stat_sample,
                 "Think Consult Chat",
                 System.currentTimeMillis());
@@ -25,7 +52,7 @@ public class Notif {
                 new Intent(context, Login.class),
                 0);
         n.setLatestEventInfo(context, "Think & Consult IM", "Secure Communication...", pendingIntent);
-
+*/
         return n;
     }
 
